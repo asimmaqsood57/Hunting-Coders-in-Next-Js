19fddfd681570adb15a7e52c82fc850e16ef9731
M.Asim Maqsood <70227239+asimmaqsood57@users.noreply.github.com>
2023-04-04 17:51:53 +0500

    end points are integrated

--- src/pages/blogpost/[slug].js
+++ src/pages/blogpost/[slug].js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import styles from "../../styles/BlogPost.module.css";
 
 const slug = () => {
   const router = useRouter();
   const { slug } = router.query;
+  const [blog, setBlog] = useState({});
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/api/getblog?slug=${slug}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setBlog(data);
+      });
+  }, []);
+
   return (
     <div className={styles.container}>
-      <h1>Title of the page {slug}</h1>
+      <h1>{blog?.title}</h1>
       <div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-          similique, quibusdam molestiae ipsa cum unde. Eaque quasi accusamus,
-          deleniti, expedita voluptates ipsa repudiandae consequatur saepe et
-          sint veritatis natus! Et!
-        </p>
+        <p>{blog?.description}</p>
       </div>
     </div>
   );
