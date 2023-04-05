dd954eef7025f783548593b2f32efc13afcee5f8
M.Asim Maqsood <70227239+asimmaqsood57@users.noreply.github.com>
2023-04-05 13:48:28 +0500

    contact api is integrated

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "../styles/Contact.module.css";
 
 const Contact = () => {
-  return <div>contact</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      name,
+      email,
+      message,
+    };
+
+    fetch("http://localhost:3000/api/postContact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  return (
+    <div className={styles.container}>
+      <h1>Contact Us</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
+            Email
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            id="exampleInputEmail1"
+            name="email"
+            aria-describedby="emailHelp"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
+            Message
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            name="message"
+            aria-describedby="emailHelp"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
